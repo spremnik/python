e4894422f9a8555b683dc7e32bbc79a088939368
Algebra NKT <138808102+spremnik@users.noreply.github.com>
2024-09-12 02:25:41 +0200

    Add files via upload

---
+++ PNG/png_buka.py
@@ -0,0 +1,29 @@
+import random
+import struct
+import zlib
+import os
+
+podaci = b''
+v = 320*240
+for tocka in range(v):
+    podaci += struct.pack("!h", random.randrange(255))
+
+def putanja(datoteka):
+    return r'{}'.format(os.path.dirname(os.path.abspath(__file__))) + f'{chr(92) if os.name == "nt" else "/"}{datoteka.replace("/",chr(92)) if os.name == "nt" else datoteka.replace(chr(92),"/")}'
+
+def pisi_datoteku(naziv, podaci=''):
+    datoteka = open(putanja(naziv), 'wb')
+    datoteka.write(podaci)
+    datoteka.close()
+
+def napravi_png(visina, sirina, podaci):
+    png_oznaka = b'\x89PNG\r\n\x1a\n'
+    ihdr_komad = b'IHDR' + struct.pack("!I",sirina) + struct.pack("!I", visina) + b'\x08\x00\x00\x00\x00'
+    ihdr_komad = struct.pack("!I",13) + ihdr_komad + struct.pack("!I", zlib.crc32(ihdr_komad))
+    sazeti_podaci = zlib.compress(podaci)
+    idat_komad = b'IDAT' + sazeti_podaci 
+    idat_komad = struct.pack("!I",len(sazeti_podaci)) + idat_komad + struct.pack("!I", zlib.crc32(idat_komad))
+    iend_komad = struct.pack("!I",0) + b'IEND' + struct.pack("!I", zlib.crc32(b'IEND'))
+    return png_oznaka + ihdr_komad + idat_komad + iend_komad
+
+pisi_datoteku('buka.png', napravi_png(240,320,podaci))
